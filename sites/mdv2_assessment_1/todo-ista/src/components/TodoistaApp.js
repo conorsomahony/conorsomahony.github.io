d02463791f76afc5881bdb88a0fac1324840d6dd
conorsomahony <conor.s.omahony@gmail.com>
2018-05-02 21:00:11 +0100

    added semantic-ui for CSS and theming

--- sites/mdv2_assessment_1/todo-ista/src/components/TodoistaApp.js
+++ sites/mdv2_assessment_1/todo-ista/src/components/TodoistaApp.js
@@ -5,17 +5,18 @@
  * Date: 02/05/2018
  * Refs:
  */
-import React, { Component, Fragment } from "react";
+import React, {Component, Fragment} from "react";
 import MyList from "./MyList";
 import MyListForm from "./MyListForm";
+import '../semantic/dist/semantic.min.css';
 
 class TodoistaApp extends Component {
   render() {
     return (
       <Fragment>
         <p>Welcome to TodoistaApp</p>
-        <MyListForm />
-        <MyList />
+        <MyListForm/>
+        <MyList/>
       </Fragment>
     );
   }
