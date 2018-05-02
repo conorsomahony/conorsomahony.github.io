610503814fca52a3b19f33240d77a841dc40a4a4
conorsomahony <conor.s.omahony@gmail.com>
2018-05-02 14:14:10 +0100

    Created components folder, added skeleton js component files for basic app components

---
+++ sites/mdv2_assessment_1/todo-ista/src/components/MyList.js
@@ -0,0 +1,12 @@
+/**
+ * Author: Conor O'Mahony
+ * Assignment: Mobile and Device Development II - Assessment 1
+ * Student ID: STU-00001490
+ * Date: 02/05/2018
+ * Refs:
+ */
+import React, { Component } from "react";
+
+class MyList extends React.Component {}
+
+export default MyList;
---
+++ sites/mdv2_assessment_1/todo-ista/src/components/TodoistaApp.js
@@ -0,0 +1,16 @@
+/**
+ * Author: Conor O'Mahony
+ * Assignment: Mobile and Device Development II - Assessment 1
+ * Student ID: STU-00001490
+ * Date: 02/05/2018
+ * Refs:
+ */
+import React, { Component } from "react";
+
+class TodoistaApp extends Component {
+  render() {
+    return <p>Welcome to TodoistaApp</p>;
+  }
+}
+
+export default TodoistaApp;
