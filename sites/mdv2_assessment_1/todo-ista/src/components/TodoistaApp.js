67f72f8027794aede14c56f5ad94661e20457cd6
conorsomahony <conor.s.omahony@gmail.com>
2018-05-03 11:52:20 +0100

    start building out the top-level app component

--- sites/mdv2_assessment_1/todo-ista/src/components/TodoistaApp.js
+++ sites/mdv2_assessment_1/todo-ista/src/components/TodoistaApp.js
@@ -5,19 +5,58 @@
  * Date: 02/05/2018
  * Refs:
  */
+
+// React Components
 import React, {Component} from "react";
+
+// My Components
 import MyList from "./MyList";
 import MyListItemForm from "./MyListItemForm";
-import GrommetApp from "grommet/components/App";
+
+// Grommet Components
+import App from "grommet/components/App";
+import Box from "grommet/components/Box";
+import Header from "grommet/components/Header";
+import Footer from "grommet/components/Footer";
+import Heading from "grommet/components/Heading";
+import Section from "grommet/components/Section";
+import Title from "grommet/components/Title";
+import Paragraph from "grommet/components/Paragraph";
+import Article from "grommet/components/Article";
 
 class TodoistaApp extends Component {
   render() {
     return (
-      <GrommetApp>
-        <p>Welcome to TodoistaApp</p>
-        <MyListItemForm/>
-        <MyList/>
-      </GrommetApp>
+      <App>
+        <Article>
+          <Header>
+            <Heading>Welcome to Todo-ista!</Heading>
+          </Header>
+          <Section>
+            <Box colorIndex='neutral-1' justify='center' align='center' pad='medium'>
+              <MyListItemForm/>
+            </Box>
+            <Box colorIndex='neutral-2' justify='center' align='center' pad='medium'>
+              <MyList/>
+            </Box>
+          </Section>
+          <Footer justify='between'>
+            <Title>
+              Todo-ista
+            </Title>
+            <Box
+              direction='row'
+              align='center'
+              pad={{
+              "between": "medium"
+            }}>
+              <Paragraph margin='none'>
+                © 2018 Todo-ista
+              </Paragraph>
+            </Box>
+          </Footer>
+        </Article>
+      </App>
     );
   }
 }
