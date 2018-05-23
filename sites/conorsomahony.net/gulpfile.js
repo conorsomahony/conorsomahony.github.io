c57867d3b5c08c7085ae7141d178cd69a7b88afd
conorsomahony <conor.s.omahony@gmail.com>
2018-05-23 16:19:51 +0100

    added autoprefixer to gulp task

--- sites/conorsomahony.net/gulpfile.js
+++ sites/conorsomahony.net/gulpfile.js
@@ -1,19 +1,32 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync');
+var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task("hello", function () {
     console.log("Hello!");
 });
 
+gulp.task('styles', function () {
+    gulp
+        .src('src/custom.css')
+        .pipe(autoprefixer({browsers: ['last 2 versions'], cascade: false}))
+        .pipe(gulp.dest('css'))
+});
+
 // Static Server
 gulp.task('watch', function () {
+    gulp.watch('src/custom.css', ['styles']);
     browserSync({
         server: {
             baseDir: './'
-        },
+        }
     });
-    gulp.watch("*.html").on('change', browserSync.reload);
-    gulp.watch("css/*.css").on('change', browserSync.reload);
+    gulp
+        .watch("*.html")
+        .on('change', browserSync.reload);
+    gulp
+        .watch("css/*.css")
+        .on('change', browserSync.reload);
 });
 
 gulp.task('default', ['watch']);
