ea5f7f4e2f4d2c920931f65034692bdd8172efa4
Conor O Mahony <conor.s.omahony@gmail.com>
2018-12-13 17:06:02 +0000

    moved todo-ista app to its own repo

--- sites/todo-ista/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "grommet/grommet.min.css";
-import "./index.css";
-import registerServiceWorker from "./registerServiceWorker";
-import TodoistaApp from "./components/TodoistaApp";
-
-ReactDOM.render(<TodoistaApp />, document.getElementById("root"));
-registerServiceWorker();
